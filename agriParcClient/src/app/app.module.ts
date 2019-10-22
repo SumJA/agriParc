7ac055987e5bd33068615e3efb423f70b57d67d4
unknown <javaid.sumaira@gmail.com>
2019-10-22 16:27:33 +0200

    Revert "add pret form"
    This reverts commit 7759123d1c8395d65866d30def23124fb00e6a29.

--- agriParcClient/src/app/app.module.ts
+++ agriParcClient/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ToastrModule } from "ngx-toastr";
@@ -31,8 +30,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     ToastrModule.forRoot(),
     FooterModule,
     FixedPluginModule,
-    HttpClientModule,
-    BrowserModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
