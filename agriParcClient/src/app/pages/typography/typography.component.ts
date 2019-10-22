b525a5c37c7623c2d7d870cf30249fec2c7a52a7
unknown <bruna.jerome@hotmail.fr>
2019-10-22 15:27:42 +0200

    final front

--- agriParcClient/src/app/pages/typography/typography.component.ts
+++ agriParcClient/src/app/pages/typography/typography.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MaterielService } from 'app/services/materiels/materiel.service';
+import { Materiels } from 'app/models/materiels';
+import { Users } from 'app/models/users';
+import { UsersService } from 'app/services/users/users.service';
 
 @Component({
     selector: 'typography-cmp',
@@ -6,4 +10,24 @@ import { Component } from '@angular/core';
     templateUrl: 'typography.component.html'
 })
 
-export class TypographyComponent{}
+export class TypographyComponent{
+    public materiel: Materiels;
+    public errorMessage: string = null;
+
+    constructor(
+        private _materielsService: MaterielService, 
+        private _userService: UsersService){ 
+    }
+
+    ngOnInit() {
+
+    }
+
+    public get materielsService(): MaterielService {
+        return this._materielsService;
+    }
+
+    public get userService(): UsersService {
+        return this._userService;
+    }
+}
